f0a7032417d65c7c581d8a0fe36ca267aed916bd
[James Puckett] <[jcp0605@tamu.edu]>
2024-11-08 19:19:54 +0000

    ResNet18 without residual connection

--- NetWork.py
+++ NetWork.py
@@ -46,10 +46,10 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
 
-        if self.downsample is not None:
-            identity = self.downsample(x)
+        # if self.downsample is not None:
+        #     identity = self.downsample(x)
 
-        out += identity
+        # out += identity
         out = self.relu(out)
 
         return out
